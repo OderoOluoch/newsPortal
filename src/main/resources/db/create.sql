9d130f5f65abe47a7ba8fd427f2bcdd792c51e76
OderoOluoch <oderoluoch@gmail.com>
2021-06-22 11:10:50 +0300

    Set up sql creator

--- src/main/resources/db/create.sql
+++ src/main/resources/db/create.sql
@@ -5,3 +5,16 @@ CREATE TABLE IF NOT EXISTS users (
     name VARCHAR,
     deptId int
 );
+
+CREATE TABLE IF NOT EXISTS departments (
+    id int PRIMARY KEY auto_increment,
+    name VARCHAR
+);
+
+CREATE TABLE IF NOT EXISTS news (
+    id int PRIMARY KEY auto_increment,
+    title VARCHAR,
+    content VARCHAR,
+    deptid INTEGER,
+    authorid INTEGER
+);
