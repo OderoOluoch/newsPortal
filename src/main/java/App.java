12f47723ee734c971b6d2dea1809f0e8c31d6fc2
OderoOluoch <oderoluoch@gmail.com>
2021-06-23 21:48:28 +0300

    Testing all POST request end point with postman

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,8 +2,13 @@ import com.google.gson.Gson;
 import dao.Sql2oDepartmentDao;
 import dao.Sql2oNewsDao;
 import dao.Sql2oUserDao;
+import models.Department;
+import models.News;
+import models.User;
 import org.sql2o.Connection;
 import org.sql2o.Sql2o;
+
+import static spark.Spark.post;
 
 
 public class App {
@@ -17,6 +22,40 @@ public class App {
 
         String connectionString = "jdbc:h2:~/jadle.db;INIT=RUNSCRIPT from 'classpath:db/create.sql'";
         Sql2o sql2o = new Sql2o(connectionString, "", "");
+
+        departmentDao = new Sql2oDepartmentDao(sql2o);
+        newsDao = new Sql2oNewsDao(sql2o);
+        userDao = new Sql2oUserDao(sql2o);
+        conn = sql2o.open();
+
+
+        //End points for departments
+        post("/departments/new", "application/json", (req, res) -> {
+            Department department = gson.fromJson(req.body(), Department.class);
+            departmentDao.add(department);
+            res.status(201);
+            res.type("application/json");
+            return gson.toJson(department);
+        });
+
+
+        //End points for users
+        post("/users/new", "application/json", (req, res) -> {
+            User user = gson.fromJson(req.body(), User.class);
+            userDao.add(user);
+            res.status(201);
+            res.type("application/json");
+            return gson.toJson(user);
+        });
+
+        //End points for users
+        post("/news/new", "application/json", (req, res) -> {
+            News news = gson.fromJson(req.body(), News.class);
+            newsDao.add(news);
+            res.status(201);
+            res.type("application/json");
+            return gson.toJson(news);
+        });
 
     }
 }
