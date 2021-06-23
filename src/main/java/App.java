dd5564dcc640bbd8b0aa8d07529a6b9f63ae194b
OderoOluoch <oderoluoch@gmail.com>
2021-06-23 14:21:27 +0300

    Started on the UI end point

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,22 @@
+import com.google.gson.Gson;
+import dao.Sql2oDepartmentDao;
+import dao.Sql2oNewsDao;
+import dao.Sql2oUserDao;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+
+
 public class App {
     public static void main(String[] args) {
+        Sql2oDepartmentDao departmentDao;
+        Sql2oNewsDao newsDao;
+        Sql2oUserDao userDao;
+
+        Connection conn;
+        Gson gson = new Gson();
+
+        String connectionString = "jdbc:h2:~/jadle.db;INIT=RUNSCRIPT from 'classpath:db/create.sql'";
+        Sql2o sql2o = new Sql2o(connectionString, "", "");
 
     }
 }
