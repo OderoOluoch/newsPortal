29ddef276c804576b3fb9f5ae4b8e13bbc7a229c
OderoOluoch <oderoluoch@gmail.com>
2021-07-06 13:37:33 +0300

    Round test on the URL end points

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,4 +1,5 @@
 import com.google.gson.Gson;
+import exceptions.ApiException;
 import dao.Sql2oDepartmentDao;
 import dao.Sql2oNewsDao;
 import dao.Sql2oUserDao;
@@ -7,6 +8,9 @@ import models.News;
 import models.User;
 import org.sql2o.Connection;
 import org.sql2o.Sql2o;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static spark.Spark.*;
 
@@ -32,7 +36,6 @@ public class App {
         get("/", "application/json", (req, res) -> {
             String welcome = "Welcome to the news portal";
             res.status(201);
-            res.type("application/json");
             return gson.toJson(welcome);
         });
 
@@ -44,13 +47,11 @@ public class App {
             Department department = gson.fromJson(req.body(), Department.class);
             departmentDao.add(department);
             res.status(201);
-            res.type("application/json");
             return gson.toJson(department);
         });
 
         //Read all departments
         get("/departments", "application/json", (req, res) -> { //accept a request in format JSON from an app
-            res.type("application/json");
             return gson.toJson(departmentDao.getAll());//send it back to be displayed
         });
 
@@ -58,26 +59,37 @@ public class App {
         get("/departments/:id", "application/json", (req, res) -> { //accept a request in format JSON from an app
             res.type("application/json");
             int departmentId = Integer.parseInt(req.params("id"));
-            res.type("application/json");
             return gson.toJson(departmentDao.findById(departmentId));
+        });
+
+        //Get all news by a specific department.
+        get("/departments/:id/news", "application/json", (req, res) -> {
+            int departmentId = Integer.parseInt(req.params("id"));
+            Department departmentToFind = departmentDao.findById(departmentId);
+            if (departmentToFind == null){
+                throw new ApiException(404, String.format("No foodtype with the id: \"%s\" exists", req.params("id")));
+            }
+            else if (departmentDao.getAllNewsForADepartment(departmentId).size()==0){
+                return "{\"message\":\"I'm sorry, but no restaurants are listed for this foodtype.\"}";
+            }
+            else {
+                return gson.toJson(departmentDao.getAllNewsForADepartment(departmentId));
+            }
         });
 
 
 
         //End points for users
-
         //Create User
         post("/users/new", "application/json", (req, res) -> {
             User user = gson.fromJson(req.body(), User.class);
             userDao.add(user);
             res.status(201);
-            res.type("application/json");
             return gson.toJson(user);
         });
 
         //Get All existing Users
         get("/users", "application/json", (req, res) -> { //accept a request in format JSON from an app
-            res.type("application/json");
             return gson.toJson(userDao.getAll());//send it back to be displayed
         });
 
@@ -85,7 +97,6 @@ public class App {
         get("/users/:id", "application/json", (req, res) -> { //accept a request in format JSON from an app
             res.type("application/json");
             int userId = Integer.parseInt(req.params("id"));
-            res.type("application/json");
             return gson.toJson(userDao.findById(userId));
         });
 
@@ -97,13 +108,11 @@ public class App {
             News news = gson.fromJson(req.body(), News.class);
             newsDao.add(news);
             res.status(201);
-            res.type("application/json");
             return gson.toJson(news);
         });
 
         //Get all news
         get("/news", "application/json", (req, res) -> { //accept a request in format JSON from an app
-            res.type("application/json");
             return gson.toJson(newsDao.getAll());//send it back to be displayed
         });
 
@@ -111,8 +120,59 @@ public class App {
         get("/news/:id", "application/json", (req, res) -> { //accept a request in format JSON from an app
             res.type("application/json");
             int newsId = Integer.parseInt(req.params("id"));
-            res.type("application/json");
             return gson.toJson(newsDao.findById(newsId));
+        });
+
+        //Add a department to news.
+        post("/news/:newsId/department/:departmentId", "application/json", (req, res) -> {
+            int newsId = Integer.parseInt(req.params("newsId"));
+            int departmentId = Integer.parseInt(req.params("departmentId"));
+            News news = newsDao.findById(newsId);
+            Department department = departmentDao.findById(departmentId);
+
+            if (news != null && department != null){
+                //both exist and can be associated - we should probably not connect things that are not here.
+                departmentDao.addDepartmentToNews(department,news);
+                res.status(201);
+                return gson.toJson(String.format("News Briefing '%s' and department '%s' have been associated",department.getName(), news.getTitle()));
+            }
+            else {
+                throw new ApiException(404, String.format("News Briefing or Department does not exist"));
+            }
+        });
+
+        //Get news based on a department
+        get("/news/:id/departments", "application/json", (req, res) -> {
+            int newsId = Integer.parseInt(req.params("id"));
+            News newsToFind = newsDao.findById(newsId);
+
+            if (newsToFind == null){
+                throw new ApiException(404, String.format("No news briefing with the id: \"%s\" exists", req.params("id")));
+            }
+            else if ( newsDao.getAllDepartmentsForNews(newsId).size()==0){
+                return "{\"message\":\"I'm sorry, but no departments are listed for this news briefing.\"}";
+            }
+            else {
+                return gson.toJson(newsDao.getAllDepartmentsForNews(newsId));
+            }
+        });
+
+
+
+        //FILTERS
+        exception(ApiException.class, (exception, req, res) -> {
+            ApiException err = exception;
+            Map<String, Object> jsonMap = new HashMap<>();
+            jsonMap.put("status", err.getStatusCode());
+            jsonMap.put("errorMessage", err.getMessage());
+            res.type("application/json");
+            res.status(err.getStatusCode());
+            res.body(gson.toJson(jsonMap));
+        });
+
+
+        after((req, res) ->{
+            res.type("application/json");
         });
 
     }
